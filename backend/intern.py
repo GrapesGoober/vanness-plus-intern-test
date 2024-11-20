0cf5eacdd73ebd41fcede99c771db57fe16f3c13
Graps <twinklierre@gmail.com>
2024-11-20 19:51:17 +0700

    #3 add api endpoint add_intern_record

---
+++ backend/intern.py
@@ -0,0 +1,52 @@
+# Author: Nachat Kaewmeesang
+# This code is used for internship for Vanness Plus Consulting Co., Ltd
+# intern.py contains business logic for managing intern list
+
+from enum import Enum
+import mysql.connector
+from pydantic import BaseModel
+from datetime import date
+
+# I'd keep the db file next to the features they're servicing for
+# The idea is that each service might have different database requirements
+# Which means they'll have their own database connections
+def connect_to_db():
+    # FYI: these MySQL environment variables are defined in compose.yaml
+    return mysql.connector.connect(
+        host="mysql",
+        user="root",
+        password="",
+        database="interns_db"
+    )
+
+# Enumeration of allowed values of intern
+# This is as described by the instructions
+# Prefer to put this into enum to auto-generate docs
+class InternRecordStatus(str, Enum):
+    NEW  = "New"
+    WIP  = "WIP"
+    WAIT = "Wait"
+    PASS = "Pass"
+    FAIL = "Fail"
+    HIRE = "Hire"
+
+# The request body definition to add a new intern record
+class AddInternRecordBody(BaseModel):
+    name:           str     = "John Doe"
+    applied_date:   date    = date(2024, 1, 1)
+    role:           str     = "Web Application Trainee"
+    status:         InternRecordStatus = InternRecordStatus.NEW
+
+def add_intern_record(body: AddInternRecordBody) -> bool:
+        
+    mydb = connect_to_db()
+    mycursor = mydb.cursor()
+    mycursor.execute("""
+        INSERT INTO `interns` 
+        (`name`, `applied_date`, `role`, `status`) 
+        VALUES ("John", "2024-1-1", "Web Dev", "New");
+    """)
+    mycursor.close()
+    mydb.commit()
+
+    return True
--- backend/main.py
+++ backend/main.py
@@ -4,32 +4,10 @@
 # main.py contains APIs for the app
 
 from fastapi import FastAPI
-from pydantic import BaseModel
-from pydantic import BaseModel
-import mysql.connector
+import intern
 
 app = FastAPI()
 
-def connect_to_db():
-    # these configs are defined in compose.yaml
-    return mysql.connector.connect(
-        # connects to mysql container, instead of normally via "localhost"
-        host="mysql",
-        user="root",
-        password="",
-        database="interns_db"
-    )
-
-class RecordTextBody(BaseModel):
-    text: str
-
-@app.post("/api/send_to_sql")
-async def send_to_sql(body: RecordTextBody):
-        
-    mydb = connect_to_db()
-    mycursor = mydb.cursor()
-    mycursor.execute("INSERT INTO `interns` (`text`) VALUES (%s)", (body.text,))
-    mycursor.close()
-    mydb.commit()
-
-    return True
+@app.post("/api/add_intern_record")
+async def add_intern_record(body: intern.AddInternRecordBody):
+    return intern.add_intern_record(body)
