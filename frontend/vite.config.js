af323cea4ab89baa2f4822eea2776b811ccfd941
Graps <twinklierre@gmail.com>
2024-11-21 22:06:42 +0700

    modified svelte container for hot reload

---
+++ frontend/vite.config.js
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    plugins: [sveltekit()],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true, // needed for the DC port mapping to work
+        strictPort: true,
+        port: 8080,
+    }
+});
