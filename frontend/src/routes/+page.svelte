2378224f6865f471fe54d7af449a3d88ff5eaafd
grapsgoober <twinklierre@gmail.com>
2024-11-22 10:38:46 +0700

    #9 add date picker component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import AddIntern from "./add_intern.svelte";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<AddIntern></AddIntern>
---
+++ frontend/src/routes/add_intern.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let dateStr: string = $state(
+        (new Date(Date.now()))
+            .toISOString()
+            .split('T')[0]
+    );
+    let date: Date = $derived(new Date(dateStr))
+</script>
+
+<input type="date" name="" id="" bind:value={dateStr}>
