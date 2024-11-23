ac15d7af11668e3224218b987149c5b88de2660b
Graps <twinklierre@gmail.com>
2024-11-23 09:32:39 +0700

    #7 add intern list UI

--- frontend/src/routes/intern_list.svelte
+++ frontend/src/routes/intern_list.svelte
@@ -3,18 +3,30 @@
     import type { InternFilter, InternInfoWithId,  } from "$lib";
     import { onMount } from "svelte";
 
+    let interns: Array<InternInfoWithId> = [];
+    let filter: InternFilter = {
+        name_contains: "",
+        applied_after: "2020-01-01",
+        applied_before: "2026-01-01",
+        status: InternStatus.HIRE
+    };
+
+    async function UpdateInternsArray() {
+        interns = await API.GetInterns(filter);
+    }
+    
     onMount(() => {
-        let filter: InternFilter = {
-            name_contains: "N",
-            applied_after: "2020-01-01",
-            applied_before: "2026-01-01",
-            status: InternStatus.HIRE
-        }
-        API.GetInterns(filter)
-        .then((interns) => {
-            interns.forEach((i) => {
-                console.log(i)
-            })
-        });
-    })
+        UpdateInternsArray();
+        setInterval(UpdateInternsArray, 5000);
+    });
+    
 </script>
+
+{#each interns as i}
+<div>
+    {i.status}
+    {i.name}
+    {i.applied_date}
+    {i.role}
+</div>    
+{/each}
